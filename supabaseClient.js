6364de98ca26b247f160502cbfaafaff2ad2d024
ayuktraj-t <ayuktraj445@gmail.com>
2026-08-24 23:35:38 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -5,8 +5,8 @@
 let supabaseClient = null;
 
 function initSupabase() {
-  const url = window.ENV ? window.ENV.SUPABASE_URL : "https://pcenxwfhavneypapwbxi.supabase.co";
-  const key = window.ENV ? window.ENV.SUPABASE_ANON_KEY : "sb_publishable_GGbmCPECiFwbpxsIppzyWg_EzgpLPtn";
+  const url = window.ENV ? window.ENV.SUPABASE_URL : "https://taspkwwknwopbhqphxng.supabase.co";
+  const key = window.ENV ? window.ENV.SUPABASE_ANON_KEY : "sb_publishable_wOsI0n5Z3Mj1i0VUiM7b4Q_dxvnmBmY";
 
   if (window.supabase && typeof window.supabase.createClient === 'function') {
     try {
